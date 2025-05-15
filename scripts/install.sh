0c21af4255d22f82ff8115ea73255b01218f8484
alizvak <168185973+alizvak@users.noreply.github.com>
2025-05-15 18:07:58 +0330

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+pip install packaging>=24.0
+# or
+pip install wheel==0.40.
 
 set -e
 
